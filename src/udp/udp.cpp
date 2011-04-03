8ed3a0a15d23571ee1c184537b42bda67ebe3fd7
Zi Yan <birdmanyan@gmail.com>
2011-04-03 18:48:33 -0500

    skeleton of udp part

---
+++ src/udp/udp.cpp
@@ -0,0 +1,142 @@
+#include "udp.h"
+
+int udp::sendPacket()
+{
+    int ret;
+    int bport;
+    int eport;
+
+    bport = packetArgs.dst_bport;
+    eport = packetArgs.dst_eport;
+
+    if (ifInitPayload == 0 || ifInitArgs == 0)
+        return -1;
+
+    memoryInit();      //step 1
+    networkInit();     //step 2
+    packetConstrIP();  //step 3
+    
+    while (libnet_plist_chain_next_pair(plist_p, &bport, &eport))
+    {
+        while (!(bport > eport) && bport != 0)
+        {
+            cport = bport++;
+
+            packetConstrUDP(); //step 3.5
+            packetChecksum();  //step 4
+            packetInject();    //step 5
+        }
+    }
+    memoryDeinit();    //step 6
+
+    return 0;
+}
+
+int udp::memoryInit();      //step 1
+{
+    int packet_size = LIBNET_IP_H + LIBNET_UDP_H + packetArgs.payload_size;
+
+    if (libnet_init_packet(packet_size, &packet) == -1)
+    {
+        libnet_error(LIBNET_ERR_FATAL, "libnet_init_packet failed\n");
+    }
+
+    return 0;
+
+}
+
+int udp::networkInit();     //step 2
+{
+    network = libnet_open_raw_sock(IPPROTO_RAW);
+    if (network == -1)
+    {
+        libnet_error(LIBNET_ERR_FATAL, "Can't open the network.\n");
+        return -1;
+    }
+
+    return 0;
+
+}
+
+int udp::packetConstrIP();    //step 3
+{
+    libnet_build_ip(LIBNET_UDP_H + packetArgs.payload_size,
+                    0,
+                    242,
+                    packetArgs.frag,
+                    packetArgs.ttl,
+                    IPPROTO_UDP,
+                    packetArgs.src_ip,
+                    packetArgs.dst_ip,
+                    NULL,
+                    0,
+                    packet);
+
+    return 0;
+}
+
+int udp::packetConstrUDP(); //step 3.5 
+{
+    libnet_build_udp(packetArgs.src_port,
+                     cport,
+                     payload,
+                     packetArgs.payload_size,
+                     packet + LIBNET_IP_H);
+
+    return 0;
+
+}
+
+int udp::packetChecksum();  //step 4
+{
+    if (libnet_do_checksum(packet, IPPROTO_UDP, LIBNET_UDP_H) == -1)
+    {
+        libnet_error(LIBNET_ERR_FATAL, "udp header checksum error\n");
+        return -1;
+    }
+
+    return 0;
+
+}
+
+int udp::packetInject();    //step 5
+{
+    int ret;
+
+    ret = libnet_write_ip(network, packet, packet_size);
+
+    if (ret < packet_size)
+    {
+        libnet_error(LN_ERR_WARNING, 
+                     "libnet_write_ip only wrote %d bytes\n", ret);
+    }
+    else
+    {
+        cout<<"construction and injection completed, wrote all "
+            <<ret
+            <<" bytes\n";
+    }
+
+}
+
+int udp::memoryDeinit();    //step 6
+{
+    /*
+     *  Shut down the interface.
+     */
+     if (libnet_close_raw_sock(network) == -1)
+     {
+         libnet_error(LN_ERR_WARNING, 
+                      "libnet_close_raw_sock couldn't close the interface");
+     }
+         
+
+     /*
+     *  Free packet memory.
+     */
+     libnet_destroy_packet(&packet); 
+
+     return 0;
+
+}
+
---
+++ src/udp/udp.h
@@ -0,0 +1,96 @@
+#ifndef __UDP_H__
+#define __UDP_H__
+
+#include <libnet.h>
+#include <iostream>
+
+using std::cout;
+
+#define MAX_PAYLOAD_SZ  1500
+
+typedef struct argList{
+    int payload_size;                   /* payload size*/
+    u_long src_ip, dst_ip;              /* source ip, dest ip*/
+    u_short src_port;                   /* source port*/
+    u_short dst_bport;                  /* dest port begin*/
+    u_short dst_eport;                  /* dest port end*/
+    u_int ttl;                          /* time to live*/
+    u_int frag;                         /* framentation bit*/
+
+
+} argList;
+
+class udp
+{
+private:
+    int packet_size;                    /* packet size*/
+    int network;
+    
+    u_char payload[MAX_PAYLOAD_SZ];     /* packet payload*/
+    u_char *packet;                     /* pointer to packet buffer*/
+    char err_buf[LIBNET_ERRBUF_SIZE];   /* error buffer*/
+    struct libnet_plist_chain plist;    /* plist chain*/
+    struct libnet_plist_chain *plist_p; /* plist chain pointer*/
+
+    argList packetArgs;
+    u_short cport;                      /* current dst port*/
+
+    u_int ifInitPayload;
+    u_int ifInitArgs;
+
+public:
+    udp():ifInitPayload(0), ifInitArgs(0){};
+    ~udp(){};
+
+public:
+    int setPayload(u_char *aPayload, int len = -1)
+    {
+        if (len < 0 || len > MAX_PAYLOAD_SZ)
+            return -1;
+
+        memset(payload, 0, MAX_PAYLOAD_SZ);
+        memcpy(payload, aPayload, len);
+        packetArgs.packet_size = len;
+
+        ifInitPayload = 1;
+
+        return 0;
+    };
+    void setArgs(u_int fragm, u_int ttl, 
+                 u_long srcIP, u_long dstIP,
+                 u_short srcPort, u_short dstPortb, u_short dstPorte)
+    {
+        setFragmentation(fragm);
+        setTTl(ttl);
+        setSrcIP(srcIP);
+        setDstIP(dstIP);
+        setSrcPort(srcPort);
+        setDstPortRange(dstPortb, dstPorte);
+
+        ifInitArgs = 1;
+    };
+    int sendPacket();
+private:
+    void setFragmentation(u_int flag){ packetArgs.frag = flag; };
+    void setTTL(u_int flag){ packetArgs.ttl = flag; };
+    void setSrcIP(u_long arg){ packetArgs.src_ip = arg; };
+    void setDstIP(u_long arg){ packetArgs.dst_ip = arg; };
+    void setSrcPort(u_short arg){ packetArgs.src_port = arg; };
+    void setDstPort(u_short begin, u_short end)
+    { packetArgs.dst_bport = begin; packetArgs.dst_eport = end; };
+    
+
+private:
+    int memoryInit();      //step 1
+    int networkInit();     //step 2
+    int packetConstrIP();  //step 3
+    int packetConstrUDP(); //step 3.5 
+    int packetChecksum();  //step 4
+    int packetInject();    //step 5
+    int memoryDeinit();    //step 6
+        
+    
+};
+
+
+#endif
